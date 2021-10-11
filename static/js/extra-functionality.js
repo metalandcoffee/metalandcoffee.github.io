6f0cd9898a9e9817378ddddde5ed14b5d2b1976e
FrostyyPaws <thefrostyypaws@gmail.com>
2021-10-11 00:01:49 -0400

    add: base video bg setup

--- static/js/extra-functionality.js
+++ static/js/extra-functionality.js
@@ -9,6 +9,10 @@ function identifyWeatherFromForecastString(forecast){
     if (forecast.includes('clouds')){ 
 
     }
+    if (forecast.includes('clear sky')){ 
+        weather = "sun";
+    }
+
 }
 function changeBackgroundToCurrentWeather(weather){ 
     let backgroundToUse = "";
@@ -34,6 +38,19 @@ function listenForForecastContentChanges(changesList, observerObj){
 let forecastDiv = $('.kJagFs');
 let forecastObserver = new MutationObserver(listenForForecastContentChanges)
 forecastObserver.observe(forecastDiv,{attributes: true, childList: true, subtree: true})
+let video = document.createElement('video');
+video.src = "/static/media/Sunny.mp4";
+video.muted = true;
+video.loop = true;
+video.style.opacity = 0.2;
+video.style.position = "fixed";
+video.style.top = "0";
+video.style.left = "0";
+video.style.zIndex = -10;
+video.style.height = "100%";
+video.style.minHeight = "100vh";
+$('body').appendChild(video);
+video.play();
 //$('body').style.background = "purple url('/static/media/clouds.png')"
 
 /*
