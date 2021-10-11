3ee1f3c3d8bef53ab401a7252d4705f204be2461
FrostyyPaws <thefrostyypaws@gmail.com>
2021-10-10 23:37:20 -0400

    add: refined dynamic background change framework

--- static/js/extra-functionality.js
+++ static/js/extra-functionality.js
@@ -5,23 +5,36 @@ function $$(target){
     return document.querySelectorAll(target);
 }
 function identifyWeatherFromForecastString(forecast){ 
+    let weather = "";
+    if (forecast.includes('clouds')){ 
 
+    }
 }
 function changeBackgroundToCurrentWeather(weather){ 
-    if (weather == "clouds"){}
-    if (weather == "rain"){}
-    if (weather == "sun"){}
-    if (weather == "snow"){}
+    let backgroundToUse = "";
+    if (weather == "clouds"){
+        backgroundToUse = "";
+    }
+    if (weather == "rain"){
+        backgroundToUse = "";
+    }
+    if (weather == "sun"){
+        backgroundToUse = "";
+    }
+    if (weather == "snow"){
+        backgroundToUse = "";
+    }
 }
 function listenForForecastContentChanges(changesList, observerObj){ 
     let forecastTextElement = changesList[changesList.length-1].target;
     let forecast = forecastTextElement.textContent;
-
+    let weather = identifyWeatherFromForecastString(forecast);
+    changeBackgroundToCurrentWeather(weather); 
 }
 let forecastDiv = $('.kJagFs');
 let forecastObserver = new MutationObserver(listenForForecastContentChanges)
 forecastObserver.observe(forecastDiv,{attributes: true, childList: true, subtree: true})
-
+//$('body').style.background = "purple url('/static/media/clouds.png')"
 
 /*
 $('.kJagFs').addEventListener('', event=>{ 
